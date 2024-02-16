33402210dd61cb853a77582f0ded20ecb3a306b7
Marina Bulakh <bulahmarina@knu.ua>
2024-02-16 18:59:08 +0200

    add ghost_draw

--- ghost.py
+++ ghost.py
@@ -1,4 +1,5 @@
 import main
+import pygame
 class Ghost:
    def __init__(self, x_coord, y_coord, target, speed, img, direct, dead, box, id):
         """
@@ -28,6 +29,34 @@ class Ghost:
         self.id = id
         self.turns, self.in_box = self.check_collisions()
         self.rect = self.draw()
+        
+        
+   def draw(self):
+      """
+      Малює привид на екрані залежно від його поточного стану.
+
+      Параметри:
+      - self: Екземпляр класу привида.
+    
+      Що робить:
+      Функція відповідає за малювання зображення привида на екрані відповідно до його поточного стану. 
+      Залежно від наявності активного powerup та стану привида (живий, мертвий, в стані паніки), 
+      вона вибирає відповідне зображення для малювання.
+
+      Що повертає:
+      Повертає прямокутник, який обмежує область, де знаходиться привид на екрані. Це може бути
+      корисно для подальшої обробки зіткнень або інших операцій гри. Якщо привид не малюється
+      на екрані у поточному стані, повертається None.
+      """
+      if (not powerup and not self.dead) or (eaten_ghost[self.id] and powerup and not self.dead):
+         screen.blit(self.img, (self.x_pos, self.y_pos))
+      elif powerup and not self.dead and not eaten_ghost[self.id]:
+         screen.blit(spooked_img, (self.x_pos, self.y_pos))
+      else:
+         screen.blit(dead_img, (self.x_pos, self.y_pos))
+         ghost_rect = pygame.rect.Rect((self.center_x - 18, self.center_y - 18), (36, 36))
+      return ghost_rect
+
         
         
        
