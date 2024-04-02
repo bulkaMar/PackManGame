2a65e56e0f1a72ee58035d5f3fe51bd5cd34e71d
Marina Bulakh <bulahmarina@knu.ua>
2024-04-02 17:39:07 +0300

    small chaneges in function draw for ghost

--- main.py
+++ main.py
@@ -103,19 +103,22 @@ class Ghost:
         self.in_box = box
         self.id = id
         self.turns, self.in_box = self.check_collisions()
-        self.rect = self.draw()
+        self.rect = self.draw(screen)
+        
         
     
     
-    def draw(self):
+    def draw(self, screen):
+        screen = pygame.display.get_surface()
         if (not powerup and not self.dead) or (eaten_ghost[self.id] and powerup and not self.dead):
-            screen.blit(self.img, (self.x_pos, self.y_pos))
+           screen.blit(self.img, (self.x_pos, self.y_pos))
         elif powerup and not self.dead and not eaten_ghost[self.id]:
-            screen.blit(spooked_img, (self.x_pos, self.y_pos))
+           screen.blit(spooked_img, (self.x_pos, self.y_pos))
         else:
-            screen.blit(dead_img, (self.x_pos, self.y_pos))
+           screen.blit(dead_img, (self.x_pos, self.y_pos))
         ghost_rect = pygame.rect.Rect((self.center_x - 18, self.center_y - 18), (36, 36))
         return ghost_rect
+
 
 
 
